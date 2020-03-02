a8de9104623f18c9d1ebf721b5881f436e251030
Neo191104 <57343312+Neo191104@users.noreply.github.com>
2020-03-02 11:19:12 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("exercise");
         System.out.println("gengxin");
+        System.out.println("远程更新");
     }
 }
